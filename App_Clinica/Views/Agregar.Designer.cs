62840f265aee57a79ddff716091aa55751991aa8
Elbin01 <elbindanielrivas@gmail.com>
2025-05-16 08:17:57 -0600

    FORM

---
+++ App_Clinica/Views/Agregar.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace App_Clinica.Views
+{
+    partial class Agregar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            textBox1 = new TextBox();
+            label1 = new Label();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(textBox1);
+            panel1.Location = new Point(182, 133);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(410, 127);
+            panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(156, 74);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(76, 21);
+            label1.Name = "label1";
+            label1.Size = new Size(262, 20);
+            label1.TabIndex = 1;
+            label1.Text = "MY FAVORITE MUSIC IS LA VACA LOLA";
+            // 
+            // Agregar
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "Agregar";
+            Text = "Agregar";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
