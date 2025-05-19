252580cfaa4af2bc77f014b1446bd2f1a3c33131
Mordo007 <fredericktareas16@gmail.com>
2025-05-19 15:53:49 -0600

    Creación de la clase "Conexión"

---
+++ App_Clinica/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace App_Clinica
+{
+    public class Conexion
+    {
+        private SqlConnection conexion;
+        private string cadenaConexion;
+
+        public Conexion()
+        {
+            cadenaConexion = "Server=DESKTOP-1LBQ6O2;Database=BDPrueba;User Id=sa;Password=V7#pRw9z2@LmXQf&;";
+            conexion = new SqlConnection(cadenaConexion);
+        }
+
+        public SqlConnection AbrirConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+            return conexion;
+        }
+
+        public void CerrarConexion()
+        {
+            if (conexion.State == System.Data.ConnectionState.Open)
+                conexion.Close();
+        }
+    }
+
+}
